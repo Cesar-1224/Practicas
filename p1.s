0ca138bedfb25f8fb84133fe8278a68e548ed56a
Cesar Michael Perez Garcia <118482822+Cesar-1224@users.noreply.github.com>
2025-04-09 23:23:27 -0700

    Update p1.s

--- p1.s
+++ p1.s
@@ -1,4 +1,5 @@
 # =========================================
+# Grabacion asciinema:     https://asciinema.org/a/BrIsB1TIBbo5Td4TWwobCmuYh
 # Programa: Conversión de Celsius a Fahrenheit
 # Autor: Perez Garcia Cesar Michael
 # Descripción: Solicita una temperatura en grados Celsius, la convierte a Fahrenheit y muestra el resultado
